2ba2d52be769f8542110a87ac1c760823dcd9701
riya2104 <73233948+riya2104@users.noreply.github.com>
2020-10-23 17:21:37 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,21 +1,44 @@
 <?php
-	$firstName = $_POST['firstName'];
-	$lastName = $_POST['lastName'];
-	$gender = $_POST['gender'];
-	$symtoms = $_POST['symtoms'];
-	$temperature = $_POST['temperature'];
-	$glucoseLevel = $_POST['glucoseLevel'];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$symptoms = $_POST['symptoms'];
+$temperature = $_POST['temperature'];
+$glucoseLevel =$_POST['glucoseLevel'];
+
 
 	//Database connection
-	$conn = new mysqli('localhost','root','','test');
-	if($conn->connect_error){
-		die('Connection Failed : '.$conn->connect_error);
-	}else{
-		$stmt = $conn->prepare("insert into patient(firstName, lastName, gender, symtoms, temperature, glucoseLevel) values(?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("ssssii",$firstName, $lastName, $gender, $symtoms, $temperature, $glucoseLevel);
-		$stmt->execute();
-		echo "Data Entered...";
-		$stmt->close();
-		$conn->close();
-	}
+//$con = mysqli_connect("olcalhost","root","root","test");
+echo $firstName;
+$conn = new mysqli("localhost","root","","test");
+if ($conn -> connect_error) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+}
+
+//mysqli_select_db("test",$con);
+// $query = "INSERT INTO doctor (firstName, lastName, gender, symptoms, temperature, glucoseLevel) VALUES ('Riya','Tayade','f', 'fever', 12,4006);";
+
+
+ echo gettype($firstName);
+ echo gettype($lastName);
+ echo gettype($symptoms);
+ echo gettype($temperature);
+ echo gettype($glucoseLevel);
+
+
+$query='INSERT INTO patient VALUES(\'' . $firstName . '\',\'' . $lastName .'\',\''. $symptoms . '\',\'' . $temperature .'\',\''. $glucoseLevel . '\');';
+if ($conn->query($query) === TRUE) {
+header('location:http://localhost/Kratin/display.php');
+} else {
+  echo "Error: " . $query . "<br>" . $conn->error;
+}
+
+//$result=$conn->query($query);
+// if(!mysql_query($query,$con))
+// {
+// die('Error...'.mysqli_error(null));
+// }else
+// {
+// echo "Data Inserted";
+// }
+
 ?>
