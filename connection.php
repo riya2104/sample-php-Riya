6ea1e3cc6a101d4aa82a5a73bbfb9f8dc68a0da3
riya2104 <73233948+riya2104@users.noreply.github.com>
2020-10-23 17:23:26 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
-$symtoms = $_POST['symtoms'];
-$temperature = (int)$_POST['temperature'];
-$glucoseLevel =(int) $_POST['glucoseLevel'];
+$symptoms = $_POST['symptoms'];
+$temperature = $_POST['temperature'];
+$glucoseLevel = $_POST['glucoseLevel'];
 $medication = $_POST['medication'];
 $diet = $_POST['diet'];
 
@@ -11,23 +11,25 @@ $diet = $_POST['diet'];
 //$con = mysqli_connect("olcalhost","root","root","test");
 echo $firstName;
 $conn = new mysqli("localhost","root","","test");
-if ($conn -> connect_errno) {
+if ($conn -> connect_error) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
 }
 
 //mysqli_select_db("test",$con);
-//$query = "INSERT INTO doctor (firstName, lastName, systoms, temperature, glucoseLevel, medication, diet)
+//$query = "INSERT INTO doctor (firstName, lastName, symptoms, temperature, glucoseLevel, medication, diet)
 //VALUES ('Cardinal','TomErichsen','Skagen21',12,4006,'Norway','diet');";
 
-echo gettype($temperature);
 echo gettype($firstName);
 echo gettype($lastName);
+echo gettype($symptoms);
+echo gettype($temperature);
 echo gettype($glucoseLevel);
 echo gettype($medication);
 echo gettype($diet);
-echo gettype($symtoms);
 
-$query='INSERT INTO doctor VALUES(\'' . $firstName . '\',\'' . $lastName .'\',\''. $symtoms . '\',' . $temperature . ',' . $glucoseLevel . ',\'' . $medication . '\',\'' .$diet .'\');';
+
+$query='INSERT INTO doctor VALUES(\'' . $firstName . '\',\'' . $lastName .'\',\''. $symptoms . '\',\'' . $temperature .'\',
+\''. $glucoseLevel . '\',\'' . $medication . '\',\'' .$diet .'\');';
 if ($conn->query($query) === TRUE) {
   echo "New records created successfully";
 } else {
@@ -42,4 +44,5 @@ if ($conn->query($query) === TRUE) {
 // echo "Data Inserted";
 // }
 
+?>
 ?>
